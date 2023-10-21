dd537356ccbf6b94e907e1c76a799a4c96fee9e1
brakthin123 <brakthin904@gmail.com>
2023-10-21 23:21:33 +0700

    activity_Startup.xml

---
+++ app/src/main/java/kh/edu/rupp/ite/boxify/StartupActivity.kt
@@ -0,0 +1,11 @@
+package kh.edu.rupp.ite.boxify
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StartupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_startup)
+    }
+}
